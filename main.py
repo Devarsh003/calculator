47477f2892d5341b45e2288873c63ab2043c77d6
Devarsh003 <devrshid@gmail.com>
2025-04-02 13:34:07 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# this is the interface
+
+print('calculator')
+print('sum:press 1')
+print('substaction:press 2')
+print('multiplication:press 3')
+print('division:press 4')
+
+num = int(input('press button for follwing task'))
